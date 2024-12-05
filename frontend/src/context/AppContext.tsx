708c1ca0a91137151c3b8173bd1b118501875a32
Nyakamba <omwegaenock@gmail.com>
2024-12-05 19:34:30 +0300

    Build app context

---
+++ frontend/src/context/AppContext.tsx
@@ -0,0 +1,50 @@
+import React, { useState, useContext, createContext } from "react";
+import Toast from "@/components/Toast";
+import { useQuery } from "@tanstack/react-query";
+import * as apiClient from "../api/apiClient";
+
+type ToastMessage = {
+  message: string;
+  type: "SUCCESS" | "ERROR";
+};
+
+interface AppContext {
+  showToast: (message: ToastMessage) => void;
+  isLoggedIn: boolean;
+}
+
+const AppContext = createContext<AppContext | undefined>(undefined);
+
+export const AppContextProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [toastMessage, setToastMessage] = useState<ToastMessage | null>(null);
+
+  const { isError } = useQuery({
+    queryKey: ["validateToken"],
+    queryFn: apiClient.validateToken,
+    retry: false,
+  });
+
+  return (
+    <AppContext.Provider
+      value={{ showToast: setToastMessage, isLoggedIn: !isError }}
+    >
+      {toastMessage && (
+        <Toast
+          message={toastMessage.message}
+          type={toastMessage.type}
+          onClose={() => setToastMessage(null)}
+        />
+      )}
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useAppContext = () => {
+  const context = useContext(AppContext);
+  return context as AppContext;
+};
