10f079571e5c49ba7cbf24adc7f5abfc54709e16
Nyakamba <omwegaenock@gmail.com>
2024-12-09 21:07:10 +0300

    Setting up vitest

---
+++ backend/jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('jest').Config} */
+const config = {
+  verbose: true,
+  testEnvironment: "node",
+  verbose: true,
+  transform: {},
+};
+
+export default config;
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,15 @@
+/// <reference types="vitest" />
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    include: ["src/**/*.{test,spec}.{js,ts,jsx,tsx}"],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
